f91a458bd988eba3501757a83c99851d9cc157af
uoneway <uoneway@gmail.com>
2021-04-05 12:05:18 +0900

    Fix init path as relative

--- summarizers/__init__.py
+++ summarizers/__init__.py
@@ -12,6 +12,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from summarizers.summarizers import Summarizers
+from .summarizers import Summarizers
 
 __all__ = [Summarizers]
